e958d9984aa17e820908cb91e7a27a8f41533e8d
Piotr Banasik <piotr.banasik@gmail.com>
2010-09-06 16:49:08 +0800

    initial rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EveProduction::Application.config.secret_token = '16880be0a5e5a8afae7bbb15b234f1670e50cba5fc96d518b99036a6e1c1f8a0063abb277b700c054a88a6180dc9cdd8717dd14c4804c9d74839c70b46718011'
