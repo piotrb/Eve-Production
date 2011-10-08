617cfd599a70c43b6aab12cff4d89cce5c3de462
Piotr Banasik <piotr.banasik@gmail.com>
2011-10-08 16:43:46 -0700

    .

--- config/initializers/misc.rb
+++ config/initializers/misc.rb
@@ -1,7 +1,7 @@
 require 'activerecord-import'
 
 if defined? ExceptionNotifier
-  config.middleware.use ExceptionNotifier,
+  Rails.application.config.middleware.use ExceptionNotifier,
     :email_prefix => "[EVE] ",
     :sender_address => %{"Exception Notifier" <error@t-p-l.com>},
     :exception_recipients => %w{piotr.banasik@gmail.com}
