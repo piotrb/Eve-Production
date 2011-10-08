32b1c82cd8e41d0d3fb978ae30ccb23123a635a4
Piotr Banasik <piotr.banasik@gmail.com>
2011-10-08 16:52:33 -0700

    sendgrid support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,16 @@ EveProduction::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  config.action_mailer.delivery_method = :smtp
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
